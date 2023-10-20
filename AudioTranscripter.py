02d37ff3b12eb166bee6fe142f00640cdaf6278e
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-10-20 11:08:24 +0530

    initial commit

---
+++ AudioTranscripter.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr 
+audioFile = ("sample.wav") # only converts audio present in .wav extention
+r = sr.Recognizer()
+
+with sr.AudioFile(audioFile) as source:
+    audioFile = r.record(source)
+
+try:
+    print(r.recognize_google(audioFile))
+except sr.UnknownValueError:
+    print("Google couldn't recognize audio")
+except sr.RequestError:
+    print("Check audio file once")
+    
