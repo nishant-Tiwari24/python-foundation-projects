718959682ba562ee8b0df0073c3f3a224e86b74b
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-10-25 18:49:19 +0530

    d

---
+++ TheoryOfRevolution.py
@@ -0,0 +1,4 @@
+with open('file1.txt','w') as myfile:
+    myfile.write('Hey I am a nice child')
+    print(myfile)
+myfile.close()
--- TicTacToe.py
+++ TicTacToe.py
@@ -3,8 +3,31 @@ board = numpy.array([['-','-','-'],['-','-','-'],['-','-','-']])
 p1s='X'
 p2s='O'
 
+def checkRows(symbol):
+    for i in range(3):
+        count = 0
+        for j in range(3):
+            board[i][j] == symbol
+            count = count+1
+        if count==3:
+            print(symbol,'won')
+            return True
+            
+
+def won(symbol):
+    return checkRows(symbol) or checkColumn(symbol) or checkDiagnols(symbol)
+
 def place(symbol):
     print(numpy.matrix[board])
+    while(1):
+        row = int(input('Enter the row 1 or 2 or 3: '))
+        column = int(input('Enter the column 1 or 2 or 3: '))
+        if row>0 and row<4 and column>0 and column<4 and board[row-1][column-1]=='-':
+            break
+        else:
+            print('Invalid entry')
+    symbol = board[row-1][column-1]
+    return symbol
     
 
 def play():
