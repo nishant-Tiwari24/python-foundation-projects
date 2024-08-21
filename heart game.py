64bedb1e1d1375020e90ce2e5f1ac1ead40105b9
Parth Bhatt <paarthbhatt37@gmail.com>
2024-08-22 01:05:08 +0530

    git add chess game.py git commit -m 'chess is goated' git push

---
+++ heart game.py
@@ -0,0 +1,18 @@
+import turtle
+
+s = turtle.Screen()
+s.bgcolor('black')
+pen = turtle.Turtle()
+pen.speed(1)
+pen.color('red')
+pen.begin_fill()
+pen.fillcolor('red')
+pen.left(140)
+pen.forward(180)
+pen.circle(-90, 200)
+pen.setheading(60)
+pen.circle(-90, 200)
+pen.forward(180)
+pen.end_fill()
+pen.hideturtle()
+s.mainloop()
